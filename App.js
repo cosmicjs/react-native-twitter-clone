b886f20ae4c973b629c3a4104bffc9c0153e01ea
David Martin <davidjmartin01@gmail.com>
2017-05-31 13:24:16 -0700

    fixed watchman dependency issue, added first version of login screen

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
+import Login from './app/components/login';
 
-export default class Twitter extends Component {
+export default class App extends Component {
   render() {
     return (
       <Router>
         <Scene key="root">
-          <Scene key="login" component={Login} title="Login" />
-          <Scene key="feed" component={Feed} title="Feed" />
-          <Scene key="post" component={Post} title="Post" />
+          <Scene key="login" component={Login} title="Login" initial={true} />
+          {/*<Scene key="feed" component={Feed} title="Feed" />
+          // <Scene key="post" component={Post} title="Post" />*/}
         </Scene>
       </Router>
     );
@@ -17,6 +18,5 @@ export default class Twitter extends Component {
 }
 
 
-
-AppRegistry.registerComponent('main', () => Twitter);
+AppRegistry.registerComponent('main', () => App);
 
