2a881d8d4e1e0cf1df45fb3af43452563baf24e4
David Martin <davidjmartin01@gmail.com>
2017-06-02 14:03:25 -0700

    corrected redux-logger error

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers'
 
