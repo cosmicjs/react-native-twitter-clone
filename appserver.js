4d34b3109fcd9d82722fd52d9ae2aeabde80f3a1
David Martin <davidjmartin01@gmail.com>
2017-06-18 11:07:32 -0700

    set up node server to run web sockets

---
+++ appserver.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('App.js'))
+
+const PORT = process.env.PORT || 1337
+app.set('port', PORT)
+
+const server = app.listen(
+  PORT,
+  () => {
+    const { address, port } = server.address()
+    const host = address === '::' ? 'localhost' : address
+    const urlSafeHost = host.includes(':') ? `[${host}]` : host
+    console.log(`Listening on http://${urlSafeHost}:${port}`)
+  }
+)
+
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  console.log(socket.id, ' connected')
+
+  socket.on('disconnect', () => {
+    console.log(socket.id, ' disconnected')
+  })
+})
