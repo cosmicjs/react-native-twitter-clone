d0a7da2bcba1f99ac57cca684a04bf00a13d6314
Tony Spiro <tspiro@tonyspiro.com>
2017-06-21 08:44:22 -0500

    change default bucket slug

--- app/config/cosmic.js
+++ app/config/cosmic.js
@@ -1,6 +1,6 @@
 export default {
   bucket: {
-    slug: process.env.COSMIC_BUCKET || 'react-native-cosmic-app',
+    slug: process.env.COSMIC_BUCKET || 'react-native-twitter-clone',
     read_key: process.env.COSMIC_READ_KEY,
     write_key: process.env.COSMIC_WRITE_KEY
   }
